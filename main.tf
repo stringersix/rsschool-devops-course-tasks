a430877d5c3b088292f97cbad0e4a55e905ac45f
Donik <sixstringer91@gmail.com>
2025-06-21 18:04:49 +0300

    init infrastructure

--- main.tf
+++ main.tf
@@ -12,6 +12,65 @@ terraform {
   }
 }
 
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "donik-network"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Public Subnets
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = var.azs[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "donik-subnet-public-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = var.azs[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "donik-subnet-public-2"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = var.azs[0]
+
+  tags = {
+    Name = "donik-subnet-private-1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = var.azs[1]
+
+  tags = {
+    Name = "donik-subnet-private-2"
+  }
+}
+
+
+
 provider "aws" {
   region = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
