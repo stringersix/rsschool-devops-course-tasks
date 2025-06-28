dc28981a5f31d12b142be32ef309b1a267b8c276
Donik <sixstringer91@gmail.com>
2025-06-29 00:37:32 +0300

    init task_3

--- instances.tf
+++ instances.tf
@@ -46,30 +46,3 @@ resource "aws_instance" "private_instance_1" {
     Name = "donik-private-instance-1"
   }
 }
-
-# Public instance in public-1 subnet
-# resource "aws_instance" "public_instance_1" {
-#   ami                         = data.aws_ami.amazon_linux_2.id
-#   instance_type               = "t2.micro"
-#   subnet_id                   = aws_subnet.public_1.id
-#   vpc_security_group_ids      = [aws_security_group.public_instance.id]
-#   associate_public_ip_address = true
-#   key_name                    = aws_key_pair.inner_key.key_name
-
-#   tags = {
-#     Name = "donik-public-instance-1"
-#   }
-# }
-
-# Private instance in private-1 subnet
-# resource "aws_instance" "private_instance_2" {
-#   ami                    = data.aws_ami.amazon_linux_2.id
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.private_2.id
-#   vpc_security_group_ids = [aws_security_group.private_instance.id]
-#   key_name               = aws_key_pair.inner_key.key_name
-
-#   tags = {
-#     Name = "donik-private-instance-2"
-#   }
-# }
